452c7a4da77daea69adb51b85369fcf30b169101
Alec Dye <alecdye@gmail.com>
2020-02-17 19:42:26 -0800

    restructuring components

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import TodoList from "./components/TodoList";
 import TodoForm from "./components/TodoForm";
-
+import { todoReducer, initialState } from "./reducers/todoReducer";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <TodoForm />
-      {/* <TodoList /> */}
+      {/* <TodoForm />
+      <TodoList /> */}
     </div>
   )
 }
--- todo-app/src/components/TodoForm.js
+++ todo-app/src/components/TodoForm.js
@@ -1,58 +1,80 @@
-import React, { useState, useReducer } from "react";
-import { todoReducer, initialState } from "../reducers/todoReducer";
-import TodoList from "./TodoList";
+import React, { useState } from "react";
 
 const TodoForm = () => {
-    //? state, handleChanges, handleSubmit?
-    const [state, dispatch] = useReducer(todoReducer, initialState);
-    const [newTodo, setNewTodo] = useState("");
-
-    const handleChanges = event => {
-        setNewTodo(event.target.value)
-    }
-
-    // const toggleTodo = clickedId => {
-    //     dispatch({
-    //         type: "TOGGLE_TODO", payload: clickedId
-    //     })
-    // }
-
+    const [inputState, setInputState] = useState("");
     return (
-        <div>
+        <form onSubmit={event => {
+            event.preventDefault();
+        }}>
             <input
-                className="todo-input"
                 type="text"
-                name="newTodo"
-                value={newTodo}
-                onChange={handleChanges}
-            />
-            <button
-                type="submit"
-                onClick={() => {
-                    dispatch({ type: "ADD_TODO", payload: newTodo })
+                name=""
+                value={inputState}
+                onChange={event => {
+                    setInputState(event.target.value)
                 }}
-            >
-                Add Task
-            </button>
-            {/* <TodoList /> */}
-            <div>{state.todoEntries.map(todoObj => {
-                return (
-                    // <Todo
-                    //     key={todoObj.id}
-                    //     item={todoObj.item}
-                    // />
-                    <div>
-                        <p>{todoObj.item}</p>
-                    </div>
-                )
-            })}
-                <button>
-                    Clear Completed Tasks
-            </button>
-            </div>
-        </div>
+            />
+            <button type="submit">Add Task</button>
+        </form>
     )
 }
 
-
 export default TodoForm;
+// import React, { useState, useReducer } from "react";
+// import { todoReducer, initialState } from "../reducers/todoReducer";
+// import TodoList from "./TodoList";
+
+// const TodoForm = () => {
+//     //? state, handleChanges, handleSubmit?
+//     const [state, dispatch] = useReducer(todoReducer, initialState);
+//     const [newTodo, setNewTodo] = useState("");
+
+//     const handleChanges = event => {
+//         setNewTodo(event.target.value)
+//     }
+
+//     // const toggleTodo = clickedId => {
+//     //     dispatch({
+//     //         type: "TOGGLE_TODO", payload: clickedId
+//     //     })
+//     // }
+
+//     return (
+//         <div>
+//             <input
+//                 className="todo-input"
+//                 type="text"
+//                 name="newTodo"
+//                 value={newTodo}
+//                 onChange={handleChanges}
+//             />
+//             <button
+//                 type="submit"
+//                 onClick={() => {
+//                     dispatch({ type: "ADD_TODO", payload: newTodo })
+//                 }}
+//             >
+//                 Add Task
+//             </button>
+//             {/* <TodoList /> */}
+//             <div>{state.todoEntries.map(todoObj => {
+//                 return (
+//                     // <Todo
+//                     //     key={todoObj.id}
+//                     //     item={todoObj.item}
+//                     // />
+//                     <div>
+//                         <p>{todoObj.item}</p>
+//                     </div>
+//                 )
+//             })}
+//                 <button>
+//                     Clear Completed Tasks
+//             </button>
+//             </div>
+//         </div>
+//     )
+// }
+
+
+// export default TodoForm;
